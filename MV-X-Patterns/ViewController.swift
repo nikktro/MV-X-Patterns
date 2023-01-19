60db647968a1b87d67f8597e658d416d82aae028
Nikolay Trofimov <nikktro@gmail.com>
2023-01-19 16:55:54 +0300

    Initial Commit

---
+++ MV-X-Patterns/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MV-X-Patterns
+//
+//  Created by Nikolay Trofimov on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
