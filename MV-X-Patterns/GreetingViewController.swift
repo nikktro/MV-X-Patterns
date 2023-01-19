384116515e90b76e209fd99243500646ae715b8a
Nikolay Trofimov <nikktro@gmail.com>
2023-01-19 17:04:13 +0300

    initial project state

---
+++ MV-X-Patterns/GreetingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GreetingViewController.swift
+//  MV-X-Patterns
+//
+//  Created by Nikolay Trofimov on 19.01.2023.
+//
+
+import UIKit
+
+
+class GreetingViewController: UIViewController {
+
+    @IBOutlet var greetingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func showGreetingPressed() {
+    }
+    
+}
+
---
+++ MV-X-Patterns/Model.swift
@@ -0,0 +1,13 @@
+//
+//  Model.swift
+//  MV-X-Patterns
+//
+//  Created by Nikolay Trofimov on 19.01.2023.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let surname: String
+}
