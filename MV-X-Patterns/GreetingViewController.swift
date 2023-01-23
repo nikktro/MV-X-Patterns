d1385fe0434303b243c7f4112bc25b19c219d840
Nikolay Trofimov <nikktro@gmail.com>
2023-01-23 23:31:05 +0300

    MVVM

--- MV-X-Patterns/GreetingViewController.swift
+++ MV-X-Patterns/GreetingViewController.swift
@@ -12,12 +12,23 @@ class GreetingViewController: UIViewController {
 
     @IBOutlet var greetingLabel: UILabel!
     
+    private var viewModel: GreetingViewModelProtocol! {
+        didSet {
+            viewModel.greetingDidChange = { [unowned self] viewModel in
+                self.greetingLabel.text = viewModel.greeting
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let person = Person(name: "Tim", surname: "Cook")
+        viewModel = GreetingViewModel(person: person)
     }
 
     @IBAction func showGreetingPressed() {
+        viewModel.showGreeting()
     }
     
 }
---
+++ MV-X-Patterns/GreetingViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  GreetingViewModel.swift
+//  MV-X-Patterns
+//
+//  Created by Nikolay Trofimov on 23.01.2023.
+//
+
+import Foundation
+
+protocol GreetingViewModelProtocol {
+    var greeting: String? { get }
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)? { get set }
+    init(person: Person)
+    func showGreeting()
+}
+
+class GreetingViewModel: GreetingViewModelProtocol {
+    var greeting: String? {
+        didSet {
+            greetingDidChange?(self)
+        }
+    }
+    
+    var greetingDidChange: ((GreetingViewModelProtocol) -> Void)?
+    
+    private let person: Person
+    
+    required init(person: Person) {
+        self.person = person
+    }
+    
+    func showGreeting() {
+        greeting = "Hello, \(person.name) \(person.surname)!"
+    }
+    
+}
